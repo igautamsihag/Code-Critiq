150d36b110ee14288c5ad2c22632ada0b6236eb3
Gautam Sihag <gautam@Gautams-MacBook-Pro.local>
2026-05-12 11:25:37 +1000

    fix jest lint issue

--- client/__mocks__/next/image.tsx
+++ client/__mocks__/next/image.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+/* eslint-disable @next/next/no-img-element */
 export default function MockImage({ src, alt }: { src: string; alt: string }) {
   return <img src={src} alt={alt} />
 }
